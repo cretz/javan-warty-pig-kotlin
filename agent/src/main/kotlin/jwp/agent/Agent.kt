d7d7722c7d7fb358d40140fb8fc7202104196a9f
Chad Retz <chad.retz@gmail.com>
2018-01-20 14:11:08 -0600

    Removed C++ bridge and used CName to expose functions

--- agent/src/main/kotlin/jwp/agent/Agent.kt
+++ agent/src/main/kotlin/jwp/agent/Agent.kt
@@ -2,6 +2,7 @@
 package jwp.agent
 
 import jvmti.*
+import konan.internal.CName
 import kotlinx.cinterop.*
 
 // Need this global JNI env ref. Set once in init.
@@ -10,16 +11,18 @@ var jvmtiEnvRef: jvmtiEnvVar?
     set(value) { globalJvmtiEnv = value?.ptr }
 
 // Initial load of the agent to initialize some things
-fun agentOnLoad(vmPtr: RawVmPtr, optionsPtr: Long, reservedPtr: Long): jint =
+@CName("Agent_OnLoad")
+fun agentOnLoad(vmPtr: CPointer<JavaVMVar>?, optionsPtr: COpaquePointer?, reservedPtr: COpaquePointer?): jint =
     init(vmPtr)?.let { println("Error: $it"); JNI_ERR } ?: JNI_OK
 
-fun agentOnAttach(vmPtr: RawVmPtr, optionsPtr: Long, reservedPtr: Long): jint =
+@CName("Agent_OnAttach")
+fun agentOnAttach(vmPtr: CPointer<JavaVMVar>?, optionsPtr: COpaquePointer?, reservedPtr: COpaquePointer): jint =
     init(vmPtr)?.let { println("Error: $it"); JNI_ERR } ?: JNI_OK
 
 // Initialize the tracer, return error string on failure
-private fun init(vmPtr: RawVmPtr): String? = memScoped {
+private fun init(vmPtr: CPointer<JavaVMVar>?): String? = memScoped {
     // Get the JVMTI environment and set the global ref
-    val env = vmPtr.vm?.getEnv() ?: return "No environment"
+    val env = vmPtr?.pointed?.getEnv() ?: return "No environment"
     jvmtiEnvRef = env
 
     // Add some capabilities
@@ -39,40 +42,43 @@ private fun init(vmPtr: RawVmPtr): String? = memScoped {
 }
 
 // Final function run on this agent
-fun agentOnUnload(vmPtr: RawVmPtr) {
+@CName("Agent_OnUnload")
+fun agentOnUnload(vmPtr: CPointer<JavaVMVar>?) {
     jvmtiEnvRef = null
 }
 
 // Called when trace is started on the thread. Expected to be globally synchronized along with stopTrace.
-fun startTrace(jniEnvPtr: RawJniEnvPtr, threadPtr: Long) {
+@CName("Java_jwp_fuzz_JavaUtils_startJvmtiTrace")
+fun startTrace(jniEnvPtr: CPointer<JNIEnvVar>?, cls: jclass, thread: jthread) {
     val env = jvmtiEnvRef ?: error("No env")
 
     // Create the tracer state and put as a thread local
-    env.setThreadLocalStorage(threadPtr.toCPointer()!!, StableRef.create(TracerState()).asCPointer()).also {
+    env.setThreadLocalStorage(thread, StableRef.create(TracerState()).asCPointer()).also {
         require(it == JVMTI_ERROR_NONE) { "Set thread-local error code $it" }
     }
 
     // Enable stepping for this thread
-    env.setEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_SINGLE_STEP, threadPtr.toCPointer()).
-        also { if (it != JVMTI_ERROR_NONE) println("Enable event failure code: $it") }
+    env.setEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_SINGLE_STEP, thread).
+            also { if (it != JVMTI_ERROR_NONE) println("Enable event failure code: $it") }
 }
 
 // Called when trace is stopped on the thread. Expected to be globally synchronized along with startTrace.
-// This returns a pointer to the JVM array of longs.
-fun stopTrace(jniEnvPtr: RawJniEnvPtr, threadPtr: Long): Long = memScoped {
+// This returns a JVM array of longs.
+@CName("Java_jwp_fuzz_JavaUtils_stopJvmtiTrace")
+fun stopTrace(jniEnvPtr: CPointer<JNIEnvVar>?, cls: jclass, thread: jthread): jlongArray = memScoped {
     val env = jvmtiEnvRef ?: error("No env")
-    val jniEnv = jniEnvPtr.jniEnv ?: error("No JNI env")
+    val jniEnv = jniEnvPtr?.pointed ?: error("No JNI env")
 
     // Disable stepping on this thread
-    env.setEventNotificationMode(JVMTI_DISABLE, JVMTI_EVENT_SINGLE_STEP, threadPtr.toCPointer()).
+    env.setEventNotificationMode(JVMTI_DISABLE, JVMTI_EVENT_SINGLE_STEP, thread).
         also { if (it != JVMTI_ERROR_NONE) println("Disable event failure code: $it") }
 
     // Get the state
-    val stateRef = env.getThreadLocalStorage(threadPtr.toCPointer()!!)?.asStableRef<TracerState>()
+    val stateRef = env.getThreadLocalStorage(thread)?.asStableRef<TracerState>()
     val state = stateRef?.get() ?: error("No state")
 
     // Remove it from thread local storage
-    env.setThreadLocalStorage(threadPtr.toCPointer()!!, null).also {
+    env.setThreadLocalStorage(thread, null).also {
         require(it == JVMTI_ERROR_NONE) { "Set thread-local error code $it" }
     }
 
@@ -94,19 +100,23 @@ fun stopTrace(jniEnvPtr: RawJniEnvPtr, threadPtr: Long): Long = memScoped {
     stateRef.dispose()
 
     // Return the pointer
-    return tuplesJniArray.toLong()
+    return tuplesJniArray
 }
 
-fun markNonBranches(jniEnvPtr: Long, possibleLongArrayPtr: Long) {
+// Take a long array, of which every 5 represents a branch, and set them as -1 if they
+// are not really branches.
+@CName("Java_jwp_fuzz_JavaUtils_markNonBranches")
+fun markNonBranches(jniEnvPtr: CPointer<JNIEnvVar>?, cls: jclass, possible: jlongArray) {
     // Every 5 values of the long array is a branch tuple. The first and third are fromMethodId and
-    // toMethodId respectively. If they are the different, we know it is a branch and move on. If they
-    // are the same, we need to fetch the bytecodes of the method and see if they are really a branch.
-    // If they aren't, we mark all 5 pieces as -1.
+    // toMethodId respectively. The second and fourth are the to/from locations respectively. If the
+    // method IDs are the different, we know it is a branch and move on. If they are the same, we need
+    // to fetch the bytecodes of the method and see if they are really a branch. If they aren't, we
+    // mark all 5 pieces as -1.
     val env = jvmtiEnvRef ?: error("No env")
-    val jniEnv = jniEnvPtr.jniEnv ?: error("No JNI env")
+    val jniEnv = jniEnvPtr?.pointed ?: error("No JNI env")
 
     // Grab and check length
-    val len = jniEnv.getArrayLength(possibleLongArrayPtr.toCPointer()!!) ?: error("No length")
+    val len = jniEnv.getArrayLength(possible) ?: error("No length")
     if (len == 0) return
     require(len % 5 == 0) { "Unexpected array length" }
 
@@ -125,7 +135,7 @@ fun markNonBranches(jniEnvPtr: Long, possibleLongArrayPtr: Long) {
     }
 
     // Grab all array pieces as native pieces
-    val arrPieces = jniEnv.getLongArrayElements(possibleLongArrayPtr.toCPointer()!!) ?: error("No array")
+    val arrPieces = jniEnv.getLongArrayElements(possible) ?: error("No array")
     for (i in 0 until len step 5) {
         // Different from/to method? Then it must be a branch
         if (arrPieces[i] != arrPieces[i + 2]) continue
@@ -153,7 +163,7 @@ fun markNonBranches(jniEnvPtr: Long, possibleLongArrayPtr: Long) {
     // Dealloc all the fetched bytecodes
     bytecodeCache.forEach { (_, lenAndBytecodes) -> env.deallocate(lenAndBytecodes.second) }
     // Release the array, using param 0 to commit changes back to original array
-    jniEnv.releaseLongArrayElements(possibleLongArrayPtr.toCPointer()!!, arrPieces, 0);
+    jniEnv.releaseLongArrayElements(possible, arrPieces, 0);
 }
 
 // Called on each step
@@ -173,15 +183,17 @@ private fun traceStep(
     }
 }
 
-fun methodName(jniEnvPtr: RawJniEnvPtr, methodIdPtr: Long): Long {
+@CName("Java_jwp_fuzz_JavaUtils_methodName")
+fun methodName(jniEnvPtr: CPointer<JNIEnvVar>?, cls: jclass, methodIdPtr: jlong): jstring? {
     val env = jvmtiEnvRef ?: error("No env")
-    val jniEnv = jniEnvPtr.jniEnv ?: error("No JNI env")
+    val jniEnv = jniEnvPtr?.pointed ?: error("No JNI env")
     return env.getMethodNameRaw(methodIdPtr.toCPointer()!!)?.let { namePtr ->
-        jniEnv.newStringUtf(namePtr).also { env.deallocate(namePtr) }.toLong()
-    } ?: 0
+        jniEnv.newStringUtf(namePtr).also { env.deallocate(namePtr) }
+    }
 }
 
-fun declaringClass(jniEnvPtr: RawJniEnvPtr, methodIdPtr: Long): Long {
+@CName("Java_jwp_fuzz_JavaUtils_declaringClass")
+fun declaringClass(jniEnvPtr: CPointer<JNIEnvVar>?, cls: jclass, methodIdPtr: jlong): jclass? {
     val env = jvmtiEnvRef ?: error("No env")
-    return env.getMethodDeclaringClass(methodIdPtr.toCPointer()!!).toLong()
+    return env.getMethodDeclaringClass(methodIdPtr.toCPointer()!!)
 }
